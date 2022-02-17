b7bd31a58a0cea8321622028b2a7389e547fd7a3
Yana Peshko <80850818+yanapeshko@users.noreply.github.com>
2022-02-17 18:52:57 +0300

    esc+bcdrop

--- app.js
+++ app.js
@@ -1,65 +1,79 @@
 import images from './galleryItems.js';
-const refs = {
-  gallery: document.querySelector('.js-gallery'),
-  image: document.createElement('img'),
-  lightbox: document.querySelector('.lightbox'),
-  btn: document.querySelector('[data-action="close-lightbox"]'),
-  modal: document.querySelector('.lightbox__content'),
-  lightbox__image: document.querySelector('.lightbox__image'),
-};
 
-const createGalleryItem = ({ preview, original, description }) =>
-  `<li class="gallery__item">
-<a
-  class="gallery__link"
-  href=${original}
->
-  <img
-    class="gallery__image"
-    src=${preview}
-    data-source=${original}
-    alt=${description}
-  />
-</a>
+const galleryContainer = document.querySelector(".js-gallery");
+const imagesMarkup = createGalleryItemsMarkup(images);
+const lightBoxImage = document.querySelector(".js-lightbox");
+const imageItem = document.querySelector(".lightbox__image");
+const backdrop = document.querySelector(".lightbox__overlay");
+const btnClose = document.querySelector('[data-action="close-lightbox"]');
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+galleryContainer.addEventListener("click", onGalleryItemClick);
+btnClose.addEventListener("click", onHandlerClose);
+backdrop.addEventListener("click", onBackdropClick);
+// - Создание и рендер разметки по массиву данных `galleryItems` из `app.js` и
+//   предоставленному шаблону.
+function createGalleryItemsMarkup(images) {
+  return images
+    .map(({ preview, description, original }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href=${original}
+  >
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
 </li>`;
-const galleryMarkup = images.reduce(
-  (acc, item) => acc + createGalleryItem(item),
-  ''
-);
-refs.gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
-refs.image.classList.add('gallery__image');
-
-refs.gallery.addEventListener('click', onGalleryClick);
-refs.btn.addEventListener('click', onClickHandlerClose);
-refs.modal.addEventListener('click', closeLightbox);
-
-function onGalleryClick(e) {
+    })
+    .join("");
+}
+// - Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого
+//   изображения.
+function onGalleryItemClick(e) {
   e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
+  const isGalleryImageEl = e.target.classList.contains("gallery__image");
+  if (!isGalleryImageEl) {
     return;
   }
-  if (e.target.nodeName === 'IMG') {
-    refs.lightbox.classList.add('is-open');
-    refs.lightbox__image.src = e.target.getAttribute('data-source');
-    refs.lightbox__image.alt = e.target.alt;
+  // - Открытие модального окна по клику на элементе галереи.
+  if (isGalleryImageEl) {
+    window.addEventListener("keydown", onEscKeyPress);
+    lightBoxImage.classList.add("is-open");
+
+    // - Подмена значения атрибута `src` элемента `img.lightbox__image`.
+    imageItem.src = e.target.getAttribute("data-source");
+    imageItem.alt = e.target.alt;
   }
-  window.addEventListener('keyup', clickKey);
 }
 
-function onClickHandlerClose(e) {
-  e.preventDefault();
-  refs.lightbox.classList.remove('is-open');
-  refs.lightbox__image.src = '';
-  refs.lightbox__image.alt = '';
-  window.removeEventListener('keyup', clickKey);
+// - Закрытие модального окна по клику на кнопку
+//   `button[data-action="close-lightbox"]`.
+function onHandlerClose(e) {
+  // e.preventDefault();
+  window.removeEventListener("keydown", onEscKeyPress);
+  lightBoxImage.classList.remove("is-open");
+  // - Очистка значения атрибута `src` элемента `img.lightbox__image`. Это необходимо
+  //   для того, чтобы при следующем открытии модального окна, пока грузится
+  //   изображение, мы не видели предыдущее.
+  imageItem.src = "";
+  imageItem.alt = "";
 }
-
-function closeLightbox(e) {
+// Закрытие модального окна по клику на `div.lightbox__overlay`
+function onBackdropClick(e) {
   if (e.target === e.currentTarget) {
-    onClickHandlerClose();
+    onHandlerClose();
   }
 }
-
-function clickKey(e) {
-  onClickHandlerClose();
+// Закрытие модального окна по нажатию клавиши `ESC`
+function onEscKeyPress(e) {
+  console.log(e);
+  const ESC_KEY_CODE = "Escape";
+  if (e.code === ESC_KEY_CODE) {
+    onHandlerClose();
+  }
 }
